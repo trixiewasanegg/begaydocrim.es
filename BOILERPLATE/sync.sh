0e9f93383aad77bb947ac4e4f89c320467e32418
Trixie <samuelsson.bea@gmail.com>
2024-10-20 13:54:05 +0800

    Sync script updates

--- BOILERPLATE/sync.sh
+++ BOILERPLATE/sync.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 systemctl stop discordbot.service
-mkdir /tmp/begaydocrim.es/git
+mkdir -p /tmp/begaydocrim.es/git
 cd /tmp/begaydocrim.es/git
 git clone -q https://github.com/trixiewasanegg/begaydocrim.es.git
 cd begaydocrim.es/
-cp PUBLIC_HTML/* /var/www/begaydocrim.es/html
-cp DISCORD_BOT/* /var/www/begaydocrim.es/bot
-cp DISCORD_BOT/discordbot.service /etc/systemd/system
+cp -r PUBLIC_HTML/* /var/www/begaydocrim.es/html
+cp -r DISCORD_BOT/* /var/www/begaydocrim.es/bot
+sudo cp DISCORD_BOT/discordbot.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start discordbot.service
 rm -rf /tmp/begaydocrim.es/git/*
